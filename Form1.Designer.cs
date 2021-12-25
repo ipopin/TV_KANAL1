70e0066a29a0a694e9914e69a55f1f6be94f7f61
ipopin <96653234+ipopin@users.noreply.github.com>
2021-12-25 13:22:07 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 110);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(90, 139);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(90, 168);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(90, 197);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 23);
+            this.textBox6.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "NAZIV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "BROJ KANALA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "JEZIK";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "VREME OD";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "VREME DO";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 26);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "<<<<<";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 26);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "<-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(492, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 26);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "->";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(558, 84);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 26);
+            this.button4.TabIndex = 15;
+            this.button4.Text = ">>>>>";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(384, 219);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 42);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "NOVI CLAN";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(384, 267);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 41);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "OBRISI CLAN";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(384, 314);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(72, 41);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "PROMENI CLAN";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TV
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable TV_KANAL = new DataTable();
+
+        int rows = 0;
+        string cs = "Data source = LAPTOP-3F2S3EF; Initial catalog = TV_KANAL";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand naredba = new SqlCommand("insert into TV_KANAL (ID , NAZIV, BROJ_KANALA, JEZIK, VREME_OD, VREME_DO) values (" + textBox1.Text + ", '" + textBox2.Text + "' ,'" + textBox3.Text + "', '" + textBox4.Text + "' , '" + textBox5.Text + "' ,'" + textBox6.Text + "' ) ", veza);
+            veza.Open();
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from TV_KANAL", veza);
+            TV_KANAL.Clear();
+            adapter.Fill(TV_KANAL);
+            refresh(rows);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand naredba = new SqlCommand("Update TV_KANAL Set NAZIV= '" + textBox2.Text + "' , BROJ_KANALA= '" + textBox3.Text + "', JEZIK= '" + textBox4.Text + "' , VREME_OD= '" + textBox5.Text + "' , VREME_DO= '" + textBox6.Text + "'  where ID= " + textBox1.Text, veza);
+            veza.Open();
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from TV_KANAL", veza);
+            TV_KANAL.Clear();
+            adapter.Fill(TV_KANAL);
+            refresh(rows);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand naredba = new SqlCommand("delete from TV_KANAL where ID=" + textBox1.Text, veza);
+            veza.Open();
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from TV_KANAL", veza);
+            TV_KANAL.Clear();
+            adapter.Fill(TV_KANAL);
+            if (rows == TV_KANAL.Rows.Count) rows = rows - 1;
+            if (rows == 0)
+            {
+                button2.Enabled = false;
+            }
+            if (TV_KANAL.Rows.Count > 1)
+            {
+                button3.Enabled = true;
+            }
+            if (rows == TV_KANAL.Rows.Count - 1)
+            {
+                button3.Enabled = false;
+            }
+
+            refresh(rows);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            rows = 0;
+            refresh(rows);
+            button2.Enabled = false;
+            button3.Enabled = true;
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (rows > 0)
+            {
+                rows--;
+                refresh(rows);
+                button3.Enabled = true;
+            }
+            if (rows == 0)
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            rows = TV_KANAL.Rows.Count - 1;
+            refresh(rows);
+            button2.Enabled = true;
+            button3.Enabled = false;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (rows < TV_KANAL.Rows.Count - 1)
+            {
+                rows++;
+                refresh(rows);
+                button2.Enabled = true;
+            }
+            if (rows == TV_KANAL.Rows.Count - 1)
+            {
+                button3.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(cs);
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from info ", veza);
+            adapter.Fill(TV_KANAL);
+
+            refresh(rows);
+
+            if (rows == 0)
+            {
+                button1.Enabled = false;
+            }
+            if (rows == TV_KANAL.Rows.Count - 1)
+            {
+                button4.Enabled = false;
+            }
+        }
+        private void refresh(int x)
+        {
+            textBox1.Text = TV_KANAL.Rows[x]["ID"].ToString();
+            textBox2.Text = TV_KANAL.Rows[x]["NAZIV"].ToString();
+            textBox3.Text = TV_KANAL.Rows[x]["BROJ_KANALA"].ToString();
+            textBox4.Text = TV_KANAL.Rows[x]["JEZIK"].ToString();
+            textBox5.Text = TV_KANAL.Rows[x]["VREME_OD"].ToString();
+            textBox6.Text = TV_KANAL.Rows[x]["VREME_DO"].ToString();
+
+        }
+    }
+}
